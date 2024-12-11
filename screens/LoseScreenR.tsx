1684768ba0831189ea7571137588adcfc7be1986
Dionysios <dionysios8292@gmail.com>
2024-12-11 22:51:41 +0000

    test

--- screens/LoseScreenR.tsx
+++ screens/LoseScreenR.tsx
@@ -37,8 +37,14 @@ const LoseScreenR = () => {
             // margin: "auto",
             marginLeft: "auto",
             marginRight: "auto",
-            marginTop: height>960 ? height>1100? "30%":
-            "30%": height<850? "45%": '50%',
+            marginTop:
+              height > 960
+                ? height > 1100
+                  ? "30%"
+                  : "30%"
+                : height < 850
+                ? "45%"
+                : "50%",
             borderRadius: 20,
             display: "flex",
             alignContent: "center",
