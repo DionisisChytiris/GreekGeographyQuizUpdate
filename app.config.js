6ccf8182ef570700960f2cd0409d89b01481cf1a
Dionysios <dionysios8292@gmail.com>
2025-12-19 22:11:01 +0000

    ios icon

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
     slug: "GreekGeographyQuizApp",
     newArchEnabled: true,
     platforms: ["ios", "android"],
-    version: "1.0.26",
+    version: "1.0.27",
     icon: "./assets/iconApp.png",
     jsEngine: "hermes",
 
@@ -12,7 +12,7 @@ export default {
       supportsTablet: true,
       requireFullScreen: true,
       bundleIdentifier: "com.greekgeographyquizapp.dion",
-      buildNumber: "57",
+      buildNumber: "58",
       icon: "./assets/iconApp.png", // ✅ SAME file
       newArchEnabled: true,
       infoPlist: {
@@ -23,12 +23,12 @@ export default {
 
     android: {
       package: "com.greekgeographyquizapp.dion",
-      versionCode: 57,
+      versionCode: 58,
       newArchEnabled: true,
       jsEngine: "hermes",
     },
 
-    runtimeVersion: "1.0.26",
+    runtimeVersion: "1.0.27",
 
     updates: {
       url: "https://u.expo.dev/8e57705c-5ea4-459b-8705-25056f4c466f",
